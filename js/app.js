f7d53c300de43ab761743ef2dc26ff88bfb0c396
AbdullahElian1 <abodelian28@gmail.com>
2021-04-04 14:16:48 +0300

    add car shop project

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let firstName = prompt('what\s your name');
+console.log( typeof  firstName);
+
+let age = prompt('what\s your age');
+console.log( typeof  age);
+
+let typeCar = prompt('what\s your car type ');
+console.log( typeof typeCar);
+
+let favoriteCar = prompt('what\s your favorite car');
+console.log( typeof favoriteCar);
+
+alert("your name is"+ firstName + " your age is " + age + 
+" and your car type "+typeCar + " and  your favorite car is "+ favoriteCar );
+
